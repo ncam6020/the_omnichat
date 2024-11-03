8a0a91ae21888c03f358a9f275ba675c8eb84aec
ncam6020 <150554197+ncam6020@users.noreply.github.com>
2024-11-03 14:28:00 -0600

    Update meeting_details_form.py

--- meeting_details_form.py
+++ meeting_details_form.py
@@ -9,8 +9,7 @@ def render_meeting_details_form():
         project_number = st.text_input("Project Number:")
         meeting_location = st.text_input("Meeting Location:")
         attendees = st.text_area("Attendees:")
-        notes = st.text_area("Notes (You can paste bullets or lists here):")
-
+        
         submit_button = st.form_submit_button(label='Submit Meeting Details')
 
         if submit_button:
@@ -19,8 +18,7 @@ def render_meeting_details_form():
                 "Project Name": project_name,
                 "Project Number": project_number,
                 "Meeting Location": meeting_location,
-                "Attendees": attendees,
-                "Notes": notes
+                "Attendees": attendees
             }
             st.session_state.messages.append(
                 {
